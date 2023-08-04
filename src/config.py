a1f9b5bd3c839f534703b4ffb37bc894a7de67ee
Lakmal Wijethunga <128587431+lakmal-wijethunga@users.noreply.github.com>
2023-08-04 22:00:24 +0530

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Change this file name to `config.py`
+
+INPUT_FILE_PATH = 'files\Ticket_Reservation.xlsx'
+OUTPUT_FILE_PATH = 'files\output.xlsx'
+ADMIN_PASSWORD = '12345'
+DOMAIN_NAME = 'http://127.0.0.1:8000'
+
+PAGE_TITLE = 'QR Ticket'
+EVENT_NAME = 'Your Event Name'
