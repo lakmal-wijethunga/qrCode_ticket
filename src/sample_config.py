6707d17b3a459259f913f748a68adb62c4e87bb7
Lakmal Wijethunga <wijethu-pe20066@stu.kln.ac.lk>
2023-08-04 21:27:16 +0530

    change file paths

--- src/sample_config.py
+++
@@ -1,9 +0,0 @@
-# Change this file name to `config.py`
-
-INPUT_FILE_PATH = 'path/to/users.xlsx'
-OUTPUT_FILE_PATH = 'path/to/output.xlsx'
-ADMIN_PASSWORD = '1234'
-DOMAIN_NAME = 'http://127.0.0.1:8000'
-
-PAGE_TITLE = 'QR Ticket'
-EVENT_NAME = 'Your Event Name'
